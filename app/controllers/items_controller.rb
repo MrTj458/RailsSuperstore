b4919f51189aea0a51ee173a1ebf91a87419e7ab
MrTj458 <mrtj458@gmail.com>
2018-09-04 15:48:24 -0600

    Added content to items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,57 @@
 class ItemsController < ApplicationController
+  before_action :set_department
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
   def index
+    @items = @department.items
   end
 
   def show
   end
 
   def new
+    @item = @department.item.new
+    render partial: 'form'
   end
+
+  def create
+    @item = @department.item.new(item_params)
+
+    if @item.save
+      redirect_to department_item_path(@department.id, @item.id)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    render partial: 'form'
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to department_item_path(@department.id, @item.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to department_items
+  end
+
+  private
+
+    def set_department
+      @department = Department.find(params[:department_id])
+    end
+
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+    def item_params
+      params.require(:item).permit(:name, :price)
+    end
 end
